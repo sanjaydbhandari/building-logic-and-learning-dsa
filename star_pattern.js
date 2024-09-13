372b1e62a08cd37aeae3f92dc311f857754ffc2f
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-09-13 22:30:31 +0530

    added pattern 7

--- star_pattern.js
+++ star_pattern.js
@@ -98,6 +98,19 @@ function pattern6() {
   }
 }
 
+function pattern7(){
+  let n =5;
+  for(let i=0;i<n;i++){
+    for(let j=0;j<i;j++){
+      process.stdout.write(" ")
+    }
+    for(let k=0;k<n;k++){
+      process.stdout.write("*")
+    }
+    console.log()
+  }
+}
+
 // pattern1();
 // pattern2();
 // pattern2_2();
