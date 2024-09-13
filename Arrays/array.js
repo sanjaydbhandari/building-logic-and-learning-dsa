4fc2ea68fb61c66b31942136c279e60092503fe9
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-09-13 23:36:35 +0530

    add array concepts

---
+++ Arrays/array.js
@@ -0,0 +1,23 @@
+// // how to intialize an array
+// let arr = [];
+// console.log(arr)//[]
+
+// //assignment
+arr = [1,2,3,4,5,6]
+// console.log(arr)// [1,2,3,4,5,6]
+
+// how to iterate an array
+// for loop
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// // for of loop
+// for(let ele of arr){
+//     console.log(ele)
+// }
+
+// for each loop
+// arr.forEach((ele,i,arr) => {
+//     console.log(`${ele} of index ${i} in array ${arr}`)
+// });
