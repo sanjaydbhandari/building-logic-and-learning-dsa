8072e620c3fc4b0ed42f992482b60d4a8b03f956
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-09-13 23:47:50 +0530

    added few more method

--- Arrays/array.js
+++ Arrays/array.js
@@ -3,7 +3,7 @@
 // console.log(arr)//[]
 
 // //assignment
-arr = [1,2,3,4,5,6]
+arr = [1,2,3,4,5,6,1]
 // console.log(arr)// [1,2,3,4,5,6]
 
 // how to iterate an array
@@ -21,3 +21,36 @@ arr = [1,2,3,4,5,6]
 // arr.forEach((ele,i,arr) => {
 //     console.log(`${ele} of index ${i} in array ${arr}`)
 // });
+
+// // filter
+// let filteredArray = arr.filter(ele=>ele%2==0);
+// console.log(filteredArray)
+
+// // map
+// let modifiedArray = arr.map(ele=>ele*2)
+// console.log(modifiedArray)
+
+// inbuild methods
+
+// 1. push(ele)
+// arr.push(7)
+// console.log(arr.push(7))
+// console.log(arr)
+
+// 2. pop() => return pop ele
+// console.log(arr.pop())
+// console.log(arr)
+
+// 3. length
+// console.log(arr.length)
+
+// 4. splice => return remove element
+// console.log(arr.splice(1,4))
+// console.log(arr)
+
+// 5. indexOf(ele) => return index
+// console.log(arr.indexOf(4))
+
+// 6. lastIndexOf(ele) => return last index of pass element
+// console.log(arr.lastIndexOf(1))
+
